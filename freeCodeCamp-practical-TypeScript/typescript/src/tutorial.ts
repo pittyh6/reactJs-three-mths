f2b905805d9764cc850c797386632fdcd0d8c63f
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2025-01-31 14:11:58 +1100

    types and array

--- freeCodeCamp-practical-TypeScript/typescript/src/tutorial.ts
+++ freeCodeCamp-practical-TypeScript/typescript/src/tutorial.ts
@@ -1,15 +1,12 @@
-
-console.log("Typescript tutorial");
+//Types
 
 let name: string = "Priscila"
 name = "other"
 name = name.toUpperCase()
 console.log(name)
-
 let amount:number = 36
 amount = 36 -1
 console.log(amount)
-
 let isAwesome:boolean = false
 isAwesome = true
 console.log(isAwesome)
@@ -27,3 +24,54 @@ number = number - 2049
 console.log(number)
 let isValid:boolean = false
 console.log(!isValid ? "is true" : "is false")
+
+
+//--------------------------------------------------------------------
+
+//Union type
+let tax:number | string = 10
+tax = "$11"
+let requestStatus: 'pending'|'success'|'error' = "pending"
+requestStatus = 'success'
+let notSure: any = 10
+notSure= "maybe a string"
+notSure = true
+
+const books = ['1984', 'Brave new World', 'Fahrenheit 451']
+let foundBook: string | undefined;
+for( let book of books){
+    if(book === 'Brave new World'){
+        foundBook = book
+        break
+    }
+}
+console.log(foundBook);
+// Challenge
+// Create a variable orderStatus of type 'processing' | 'shipped' | 'delivered' and assign it the value 'processing'. Then, try to assign it the values 'shipped' and 'delivered'.
+// Create a variable discount of type number | string and assign it the value 20. Then, try to assign it the value '20%'.
+let orderStatus: 'processing' | 'shipped' | 'delivered' = 'processing'
+orderStatus = 'delivered'
+orderStatus = 'shipped'
+let discount:number | string = 20
+discount= '20%'
+
+
+//--------------------------------------------------------------------
+
+// Arrays
+let prices: number[] = [100, 72, 83, 76]
+prices.push(67)
+let fruits: string[] = ['apple', 'pineapple', 'strawberry']
+console.log(fruits)
+let array:(string|boolean)[]=["yes", true, "no", false]
+console.log(array)
+// Challenge
+// Create an array temperatures of type number[] and assign it some values. Then, try to add a string value to it.
+// Create an array colors of type string[] and assign it some values. Then, try to add a boolean value to it.
+// Create an array mixedArray of type (number | string)[] and assign it some values. Then, try to add a boolean value to it.
+let temperatures:number[] = [17,21,26,35]
+//temperatures.push("food")
+let colors: string[]=['orange', 'pink', 'purple']
+//colors.push(true)
+let mixedArray:(number|string)[]=[83,'old',2,'young']
+//mixedArray.push(true)
