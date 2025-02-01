312e60a096ec625547d84533335c839dee07d5cc
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2025-02-01 15:31:33 +1100

    Objects and Functions

--- freeCodeCamp-practical-TypeScript/typescript/src/tutorial.ts
+++ freeCodeCamp-practical-TypeScript/typescript/src/tutorial.ts
@@ -149,3 +149,82 @@ function calculateScore(initialScore: number, penaltyPoints: number = 0){
 
 let scoreAfterPenalty = calculateScore(100, 20)
 let scoreWithoutPenalty = calculateScore(100)
+
+//REST PARAMETER
+function sum(message:string, ...numbers:number[]):string{
+    const doubled = numbers.map((num) => num * 2)
+    console.log(doubled)
+
+    let total = numbers.reduce((prev, current) => {
+        return prev + current
+    },0)
+    return `${message}${total}`
+}
+let result = sum('The total is: ', 1, 2, 3, 4, 5)
+console.log(result)
+
+//void function means the function will not return a value
+function logMessage(message:string):void{
+    console.log(message);
+}
+logMessage('I am to sleep')
+
+
+//FUNCTION USING UNION TYPES AS FUNCTION PARAMETERS
+
+// Challenge
+// Your task is to create a function named processInput that accepts a parameter of a union type string | number. The function should behave as follows:
+
+// If the input is of type number, the function should multiply the number by 2 and log the result to the console.
+// If the input is of type string, the function should convert the string to uppercase and log the result to the console.
+
+function processInput(input:string | number){
+    if(typeof input === 'number'){
+        console.log(input * 2);
+    }
+    else{
+        console.log(input.toUpperCase());
+        
+    }
+}
+
+processInput(10)
+processInput("hiiiii")
+
+//with objects
+function createEmployee({id}:{id:number}):{id:number; isActive: boolean}{
+    return {id, isActive: id%2 ===0}
+}
+const first = createEmployee({id:1})
+const second = createEmployee({id:2})
+console.log(first, second);
+
+//with objects alternative
+function createStudent(student:{id:number; name:string}):void{
+    console.log(`Welcome to the course ${student.name.toUpperCase()}`);
+}
+const newStudent = {id: 5, name: 'anna'}
+
+createStudent(newStudent)
+createStudent({id: 1, name: 'Pitty'})
+
+// Challenge
+// Your task is to create a function named processData that accepts two parameters:
+
+// The first parameter, input, should be a union type that can be either a string or a number.
+// The second parameter, config, should be an object with a reverse property of type boolean, by default it "reverse" should be false
+// The function should behave as follows:
+
+// If input is of type number, the function should return the square of the number.
+// If input is of type string, the function should return the string in uppercase.
+// If the reverse property on the config object is true, and input is a string, the function should return the reversed string in uppercase.
+function processData(input: string | number, config: { reverse: boolean } = { reverse: false }): string | number {
+    if (typeof input === 'number') {
+        return input * input;
+    } else {
+        return config.reverse ? input.toUpperCase().split('').reverse().join('') : input.toUpperCase();
+    }
+}    
+console.log(processData(10)); // Output: 100
+console.log(processData('Hello')); // Output: HELLO
+console.log(processData('Hello', { reverse: true })); // Output: OLLEH
