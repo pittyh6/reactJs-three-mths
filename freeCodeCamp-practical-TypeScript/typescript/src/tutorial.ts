d12b2c367c5a8d905d0c3192ac2ab431ba09da3e
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2025-02-03 07:52:27 +1100

    intersection type

--- freeCodeCamp-practical-TypeScript/typescript/src/tutorial.ts
+++ freeCodeCamp-practical-TypeScript/typescript/src/tutorial.ts
@@ -302,3 +302,27 @@ let carlos:Manager = {
 }
 printStaffDetails(marta)
 printStaffDetails(carlos)
+
+//INTERSECTION TYPE
+type Book = { id: number; name: string; price: number };
+type DiscountedBook = Book & { discount: number };
+const book1: Book = {
+id: 2,
+name: 'How to Cook a Dragon',
+price: 15,
+};
+
+const book2: Book = {
+id: 3,
+name: 'The Secret Life of Unicorns',
+price: 18,
+};
+
+const discountedBook: DiscountedBook = {
+id: 4,
+name: 'Gnomes vs. Goblins: The Ultimate Guide',
+price: 25,
+discount: 0.15,
+};
+
+//TYPE ALIAS - COMPUTED PROPERTIES
