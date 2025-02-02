2283ad8a9e298dc7d8917bc7eb25645904644094
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2025-02-02 11:13:04 +1100

    type alias

--- freeCodeCamp-practical-TypeScript/typescript/src/tutorial.ts
+++ freeCodeCamp-practical-TypeScript/typescript/src/tutorial.ts
@@ -228,3 +228,77 @@ function processData(input: string | number, config: { reverse: boolean } = { re
 console.log(processData(10)); // Output: 100
 console.log(processData('Hello')); // Output: HELLO
 console.log(processData('Hello', { reverse: true })); // Output: OLLEH
+
+
+//--------------------------------------------------------------------
+
+//ALIAS AND INTERFACE
+
+type User = { id: number; name: string; isActive: boolean };
+
+const john: User = {
+    id: 1,
+    name: 'john',
+    isActive: true,
+};
+const susan: User = {
+    id: 1,
+    name: 'susan',
+    isActive: false,
+};
+
+function createUser(user: User): User {
+    console.log(`Hello there ${user.name.toUpperCase()} !!!`);
+    return user;
+}
+
+type StringOrNumber = string | number;
+let value: StringOrNumber
+value = 'hello'
+value = 1234
+
+type Theme = 'dark' | 'light'
+let theme: Theme
+theme = 'dark'
+theme = 'light'
+
+function setTheme(t:Theme){
+    theme=t
+}
+setTheme('dark')
+
+// Challenge
+// Define the Employee type: Create a type Employee with properties id (number), name (string), and department (string).
+
+// Define the Manager type: Create a type Manager with properties id (number), name (string), and employees (an array of Employee).
+
+// Create a Union Type: Define a type Staff that is a union of Employee and Manager.
+
+// Create the printStaffDetails function: This function should accept a parameter of type Staff. Inside the function, use a type guard to check if the 'employees' property exists in the passed object. If it does, print a message indicating that the person is a manager and the number of employees they manage. If it doesn't, print a message indicating that the person is an employee and the department they belong to.
+
+// Create Employee and Manager objects: Create two Employee objects. One named alice and second named steve. Also create a Manager object named bob who manages alice and steve.
+
+// Test the function: Call the printStaffDetails function with alice and bob as arguments and verify the output.
+type Employee = {id:number; name:string; department:string}
+type Manager = {id:number; name:string; employees:Employee[]}
+type Staff = Employee | Manager
+
+function printStaffDetails(staff:Staff):void{
+    if('employees' in staff){
+        console.log(`${staff.name} is a manager of ${staff.employees.length}`);
+    }else{
+        console.log(`${staff.name} is an employee in the department: ${staff.department}`);
+    }
+}
+
+let marta:Employee = {
+    id:1, name:"Marta", department:'DPC Team'
+}
+let lauren:Employee = {
+    id:2, name:"Louren", department:'ES'
+}
+let carlos:Manager = {
+    id:3, name:'Carlos', employees:[marta, lauren]
+}
+printStaffDetails(marta)
+printStaffDetails(carlos)
